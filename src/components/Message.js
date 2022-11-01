e98bd7afd58163827b2017e653ced3df9043e21b
Ayur <ayur.gonchikov@teamlead.ru>
2022-11-01 12:57:57 +0800

    Home work for lesson 2

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,9 +1,30 @@
+import { useState } from 'react'
 import style from './Message.module.css'
 
-export const Message = ({ text }) => {
+export const Message = (props) => {
+  const { data } = props
+  const [formList, setFormLst] = useState('')
+
+  const handlerSubmit = (event) => {
+    event.preventDefault()
+    setTimeout(() => {
+      alert(`Author: ${data.name}, Text submit: ${formList}`)
+    }, 1500)
+  }
+
   return (
     <>
-      <p className={style.Message}>{text}</p>
+      <form onSubmit={handlerSubmit}>
+        <input
+          className={style.Message}
+          type="text"
+          value={formList}
+          onChange={(event) => {
+            setFormLst(event.target.value)
+          }}
+        />
+        <input type="submit" value="Submit" />
+      </form>
     </>
   )
 }

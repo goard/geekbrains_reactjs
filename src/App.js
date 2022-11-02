26d2fa442f2bf17ff94bba47d113c1b83fff6652
Ayur <ayur.gonchikov@teamlead.ru>
2022-11-02 16:16:34 +0800

    Home work for lesson 3

--- src/App.js
+++ src/App.js
@@ -2,32 +2,71 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 import './App.css'
 import { Message } from './components/Message'
+import {
+  Typography,
+  Container,
+  Grid,
+  CircularProgress,
+  Box,
+  List,
+  ListItem,
+} from '@mui/material'
 
 function App() {
   const [messageList, setMessageList] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     ;(async function () {
-      const response = await fetch(
-        'https://jsonplaceholder.typicode.com/users?_start=0&_end=3'
-      )
-      const data = await response.json()
-      setMessageList(data)
+      setLoading(true)
+      try {
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/users?_start=0&_end=3'
+        )
+        const data = await response.json()
+        setMessageList(data)
+        setLoading(false)
+      } catch (error) {
+        console.error(error)
+        setLoading(false)
+      }
     })()
   }, [])
 
+  if (loading) {
+    return (
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <CircularProgress />
+      </Box>
+    )
+  }
+
   return (
-    <div className="App">
+    <Container style={{ paddingTop: '40px' }} maxWidth="md">
       {messageList.map((el) => (
-        <div key={el.id}>
-          <p>Name: {el.name}</p>
-          <p>Email: {el.email}</p>
-          <p>Phone: {el.phone}</p>
+        <List
+          key={el.id}
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <Grid>
+            <ListItem>
+              <Typography>Name: {el.name}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Email: {el.email}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Phone: {el.phone}</Typography>
+            </ListItem>
+          </Grid>
           <Message data={el} />
-          <hr />
-        </div>
+        </List>
       ))}
-    </div>
+    </Container>
   )
 }
 
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,16 +1,23 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import style from './Message.module.css'
 
 export const Message = (props) => {
   const { data } = props
+  const refInput = useRef()
   const [formList, setFormLst] = useState('')
+  const [send, setSend] = useState(0)
 
   const handlerSubmit = (event) => {
     event.preventDefault()
     setTimeout(() => {
       alert(`Author: ${data.name}, Text submit: ${formList}`)
+      setSend((prev) => prev + 1)
     }, 1500)
   }
+
+  useEffect(() => {
+    refInput.current?.focus()
+  }, [send])
 
   return (
     <>
@@ -22,6 +29,7 @@ export const Message = (props) => {
           onChange={(event) => {
             setFormLst(event.target.value)
           }}
+          ref={refInput}
         />
         <input type="submit" value="Submit" />
       </form>
